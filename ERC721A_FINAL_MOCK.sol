6e06db7752423fd029de56cb8c9205f0947f57e4
SQUIDzillaNFT <nrwisch@gmail.com>
2022-08-11 11:25:35 -0500

    ERC721A optimized Initial Commit

---
+++ ERC721A_FINAL_MOCK.sol
@@ -0,0 +1,280 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "./ERC721A.sol";
+import "./Ownable.sol";
+import "./SafeMath.sol"; 
+
+/*
+* ERC721A Gas Optimized Minting
+*  https://twitter.com/SQUIDzillaz0e
+*  Feel free to use and Modify.
+*  Change contract name "ERC721_SQUID"
+*  to your own Contract name Before Deployment
+*/
+
+
+contract ERC721_SQUID is ERC721A, Ownable {
+    using SafeMath for uint256;
+
+    /*
+     * @dev Set Initial Parameters Before deployment
+     * settings are still fully updateable after deployment
+     * Max Mint overall, Max NFTs in collection, Max Mint during Presale period
+     * as well as Private/Presale price adn Public Mint price.
+     */
+    uint256 MAX_MINTS = 100;
+    uint256 MAX_ELEMENTS = 5555;
+    uint256 public mintRate = 0.07 ether;
+    uint256 public privateMintPrice = 0.055 ether;
+    uint256 public MAX_MINT_WHITELIST = 50;
+    /*
+    * @Dev Booleans for sale states. 
+    * salesIsActive must be true in any case to mint
+    * privateSaleIsActive must be true in the case of Whitelist mints
+    * If you do not want a WL period or a Presale Period keep
+    * privateSalesIsActive set to False and it will bypass the sale period Entirely.
+    */
+    bool public saleIsActive = false;
+    bool public privateSaleIsActive = true;
+    /*
+    * @Dev Whitelist Struct and Mappings
+    * the address and amount minted to keep track of how many you may mint
+    * Stores Data about the wallet and their Mint History
+    */
+    struct Whitelist {
+        address addr;
+        uint256 claimAmount;
+        uint256 hasMinted;
+    }
+
+    mapping(address => Whitelist) public whitelist;
+
+    address[] whitelistAddr;
+
+    /*
+     * @dev Set base URI, Make sure when dpeloying if you plan to Have an 
+     * Unrevealed Sale or Period you Do not Deploy with your Revealed
+     * base URI here or they will mint With your revealed Images Showing
+     * I reccomend setting an Incorrect or an Unrevealed URI here, You can also leave
+     * it Not filled in and Update it with the setBaseURI Function at any Point
+     */
+    string public baseURI = "ipfs://URI_GOES_HERE_FOR_METADATA/";
+    
+    /*
+     * @dev Set your Collection/Contract name and Token Ticker
+     * below. Constructor Parameter cannot be changed after
+     * contract deployment.
+     */
+    constructor() ERC721A("COLLECTION_NAME_HERE", "TICKER_HERE") {}
+
+    /*
+     *  @dev
+     * Set presale price to mint per NFT
+     */
+    function setPrivateMintPrice(uint256 _price) external onlyOwner {
+        privateMintPrice = _price;
+    }
+
+    /*
+     *@dev
+     * Set publicsale price to mint per NFT
+     */
+    function setPublicMintPrice(uint256 _price) external onlyOwner {
+        mintRate = _price;
+    }
+
+    /*
+    * @dev mint funtion with _to address. no cost mint
+    *  by contract owner/deployer
+    */
+    function Devmint(uint256 quantity, address _to) external payable onlyOwner {
+        require(saleIsActive, "Sale must be active to mint");
+        require(totalSupply() + quantity <= MAX_ELEMENTS, "Not enough tokens left");
+        _safeMint(_to, quantity);
+    }
+
+    /*
+    * @dev mint function and checks for saleState and mint quantity
+    * Includes Private/public sale checks and quantity minted.
+    *
+    */    
+    function mint(uint256 quantity) external payable {
+        require(saleIsActive, "Sale must be active to mint");
+        // _safeMint's second argument now takes in a quantity, not a tokenId.
+        require(quantity + _numberMinted(msg.sender) <= MAX_MINTS, "Exceeds Max Allowed Mint Count");
+        require(totalSupply() + quantity <= MAX_ELEMENTS, "Not enough tokens left to Mint");
+
+        if(privateSaleIsActive) {
+           require(msg.value >= (privateMintPrice * quantity), "Not enough ETH sent");
+           require(quantity <= MAX_MINT_WHITELIST, "Above max Mint Whitelist count");
+           require(isWhitelisted(msg.sender), "Is not whitelisted");
+           require(
+                whitelist[msg.sender].hasMinted.add(quantity) <=
+                    MAX_MINT_WHITELIST,
+                "Exceeds Max Mint During Whitelist Period"
+            );
+            whitelist[msg.sender].hasMinted = whitelist[msg.sender]
+                .hasMinted
+                .add(quantity);
+        } else {
+        if (isWhitelisted(msg.sender)) {
+            require((balanceOf(msg.sender) - whitelist[msg.sender].hasMinted + quantity) <= MAX_MINTS, "Cant Mint any More Tokens");
+        } else {
+            require((balanceOf(msg.sender) + quantity) <= MAX_MINTS, "Cant Mint any More Tokens");
+        }
+            require(
+                (mintRate * quantity) <= msg.value,
+                "Value below price"
+            );
+        }
+
+
+        if (totalSupply() < MAX_ELEMENTS){
+        _safeMint(msg.sender, quantity);
+        }
+    }
+
+    /*
+     * @dev Set Max Mints allowed by any one single
+     * wallet during the entirety of the Mint.
+     */
+    function setMaxMints(uint256 _max) external onlyOwner {
+        MAX_MINTS = _max;
+    }
+
+    /*
+     * @dev Set new Max Mint while in Whitelist
+     * sale period. Total number of NFTs one can mint during
+     * This period per wallet.
+     */
+    function setMaxMintsWhiteList(uint256 _wlMax) external onlyOwner {
+        MAX_MINT_WHITELIST = _wlMax;
+    }
+
+    /*
+     * @dev Set new Base URI
+     * useful for setting unrevealed uri to revealed Base URI
+     * same as a reveal switch/state but not the extraness
+     */
+    function setBaseURI(string calldata newBaseURI) external onlyOwner {
+        baseURI = newBaseURI;
+    }
+
+    /*
+     * @dev returns Max allowed NFTs to be minted
+     *  on frontend/etherscan
+     */
+    function maxSupply() external view returns (uint256) {
+        return MAX_ELEMENTS;
+    }
+
+    /*
+     * @dev returns Max total mints allowed
+     * by any one wallet on frontend/etherscan
+     */
+    function maxAllowedMints() external view returns (uint256) {
+        return MAX_MINTS;
+    }
+
+    /*
+     * @dev returns Public Mint price on frontend/etherscan
+     */
+    function publicmintPrice() external view returns (uint256) {
+    return mintRate;
+    }
+
+    /*
+     * @dev Withdrawl function, Contract ETH balance
+     * to owner wallet address.
+     */
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+     /*
+     * @dev returns Base URI on frontend/etherscan
+     */
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+     /*
+     * @dev Set Public Price
+     */
+    function setPublicRate(uint256 _mintRate) public onlyOwner {
+        mintRate = _mintRate;
+    }
+
+     /*
+     * @dev Pause sale if active, make active if paused
+     */
+    function flipSaleState() public onlyOwner {
+        saleIsActive = !saleIsActive;
+    }
+    
+    /*
+    * @dev flip sale state form whitelist to public
+    *
+    */
+    function flipPrivateSaleState() public onlyOwner {
+        privateSaleIsActive = !privateSaleIsActive;
+    }
+
+    /*
+    * @dev Set Whitelist address
+    * array - format must be: ["address1","address2"]
+    * 
+    */
+    function setWhitelistAddr(address[] memory addrs) public onlyOwner {
+        whitelistAddr = addrs;
+        for (uint256 i = 0; i < whitelistAddr.length; i++) {
+            addAddressToWhitelist(whitelistAddr[i]);
+        }
+    }
+
+    /*
+    * @dev Alternative withdrawl
+    * mint funs to a specified address
+    * 
+    */
+    function altWithdraw(uint256 _amount, address payable _to)
+        external
+        onlyOwner
+    {
+        require(_amount > 0, "Withdraw must be greater than 0");
+        require(_amount <= address(this).balance, "Amount too high");
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success);
+    }
+
+    /*
+    * @dev Add a single Wallet
+    * address to whitelist
+    * 
+    */
+    function addAddressToWhitelist(address addr)
+        public
+        onlyOwner
+        returns (bool success)
+    {
+        require(!isWhitelisted(addr), "Already whitelisted");
+        whitelist[addr].addr = addr;
+        success = true;
+    }
+    
+    /*
+    * @dev return a boolean true or false if
+    * an address is whitelisted on etherscan
+    * or frontend
+    */
+    function isWhitelisted(address addr)
+        public
+        view
+        returns (bool isWhiteListed)
+    {
+        return whitelist[addr].addr == addr;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SQUID_ERC721A
  Squidzilla ERC721A Free to use Gas Optimized Batch Mint Contract
+
+Pay Attention to My comments it will guide you throughh changes needed. If using the Flattened Version the Main contract Starts at Line #1537. Learning on your own can be a bit of work so
+I tried to be as Detailed as Possible. Any questions or comments Feel free to reach out.
+
+Telegram: @SQUIDzilla
+Twitter: @SQUIDzillaz0e
+Email: nrwisch@gmail.com
+
+Written to Optimize Gas for Mandox LLC Mints - Free to Use
